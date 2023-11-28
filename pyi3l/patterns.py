66027b157cc97c37ab26c3228ed675efc8a23ed9
Vít Šesták 'v6ak <you@example.com>
2023-11-28 13:31:59 +0100

    Import and optimize patterns

--- pyi3l/patterns.py
+++ pyi3l/patterns.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from abc import ABC, abstractmethod
 from typing import List
 from .util import pcre_quote
+from functools import reduce
 
 
 class Pattern(ABC):
@@ -14,6 +15,54 @@ class Pattern(ABC):
     def __add__(self, other: "Pattern"):
         return CompoundPattern([self, other])
 
+    @staticmethod
+    def import_pattern(s):
+        if s is None:
+            return None
+        else:
+            if s.startswith("^") and s.endswith("$"):
+                return Pattern.import_pattern_part(s[1:-1]).optimize()
+            else:
+                raise ValueError("Cannot import pattern "+s)
+
+    @staticmethod
+    def import_pattern_part(s):
+        def unsupported(s):
+            raise ValueError(f"Unsupported pattern part: {s}")
+
+        def import_default(s):
+            return Literal(s[0]) + Pattern.import_pattern_part(s[1:])
+
+        def import_escaped(s):
+            if len(s) < 2:
+                raise ValueError("Missing character after escape")
+            c = s[1]
+            rest = s[2:]
+            if c.isalpha():
+                raise ValueError(f"Unsupported escape char: {c}")
+            return Literal(c) + Pattern.import_pattern_part(rest)
+
+        if s == "":
+            return Literal("")
+        else:
+            return {
+                ".": unsupported,
+                "^": unsupported,
+                "$": unsupported,
+                "*": unsupported,
+                "+": unsupported,
+                "?": unsupported,
+                "(": unsupported,
+                ")": unsupported,
+                "[": unsupported,
+                "{": unsupported,
+                "\\": import_escaped,
+                "|": unsupported,
+            }.get(s[0], import_default)(s)
+
+    def optimize(self):
+        return self
+
 @dataclass
 class Literal(Pattern):
     s: str
@@ -23,6 +72,12 @@ class Literal(Pattern):
         
     def map_chars(self, f):
         return Literal("".join(map(f, self.s)))
+
+    def __add__(self, other: "Pattern"):
+        if isinstance(other, Literal):
+            return Literal(self.s+other.s)
+        else:
+            return CompoundPattern([self, other])
 
 class Anything(Pattern):
     def to_pcre(self):
@@ -37,10 +92,15 @@ class AnyOf(Pattern):
 
     def to_pcre(self):
         return "(" + "|".join(map(lambda p: p.to_pcre(), self.variants)) + ")"
-    
+
     def map_chars(self, f):
         return AnyOf(
             variants=list(map(lambda p: p.map_chars(f), self.variants)),
+        )
+
+    def optimize(self):
+        return AnyOf(
+            variants=list(map(lambda c: c.optimize(), self.variants)),
         )
 
 @dataclass
@@ -56,6 +116,12 @@ class CompoundPattern(Pattern):
     def map_chars(self, f):
         return CompoundPattern(
             subpatterns=list(map(lambda p: p.map_chars(f), self.subpatterns)),
+        )
+
+    def optimize(self):
+        return reduce(
+            lambda a, b: a+b,
+            map(lambda c: c.optimize(), self.subpatterns)
         )
 
 @dataclass
